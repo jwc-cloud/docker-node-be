bba0cd10a31f39d33fb5cd28f8554cec836c9e50
jwc-cloud <kx7437@gmail.com>
2025-04-02 13:12:15 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // 루트 확인용
 app.get('/', (req, res) => {
